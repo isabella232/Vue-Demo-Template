f7983feb76c60888a1ff4b843d9a4da1f10dff97
Alexandre Viretti <alexou73@gmail.com>
2021-07-07 15:50:38 +0200

    feat(instantsearch): init instant search

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
-  <!-- <ais-instant-search :search-client="searchClient" index-name="rayban_merged">
-    <ais-search-box />
-    <ais-hits>
-      <div slot="item" slot-scope="{ item }">
-        <h2>{{ item.title }}</h2>
-      </div>
-    </ais-hits>
-  </ais-instant-search> -->
   <div>
-    <Header @showFederatedSearch="showFederatedSearchMethod" />
-    <HomePage v-if="!getShowFederatedSearch" />
+    <ais-instant-search
+      :search-client="searchClient"
+      index-name="rayban_merged"
+    >
+      <Header @showFederatedSearch="showFederatedSearchMethod" />
+      <HomePage v-if="!getShowFederatedSearch" />
+      <SearchPage v-else />
+    </ais-instant-search>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import HomePage from "@/components/HomePage";
+import SearchPage from "@/components/SearchPage";
 import { mapGetters } from "vuex";
+import algoliasearch from "algoliasearch/lite";
 
 export default {
   components: {
     Header,
     HomePage,
+    SearchPage,
   },
   data() {
     return {
       showFederatedSearch: true,
+      searchClient: algoliasearch(
+        "JDBD6EJM33",
+        "0fe54b2e3991d370c91376981aff9d48"
+      ),
     };
   },
   methods: {
---
+++ src/components/SearchPage/index.vue
@@ -0,0 +1,92 @@
+<template
+  ><ais-hits>
+    <!-- <div slot="item" slot-scope="{ item, index }">
+      {{ index }} - {{ item }}
+    </div> -->
+    <div class="hits-wrapper" slot="item" slot-scope="{ item }">
+      <div class="sort-and-stat">
+        <ul class="hits-list">
+          <li class="hit-list">
+            <div class="image-wrapper">
+              <img :src="item.image_link" alt="" />
+              <div class="infos">
+                <!-- <h3><Highlight hit="{hit}" attribute="title" /></h3> -->
+                <p>{{ item.title }}</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </ais-hits></template
+>
+<script></script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables/variables.scss";
+@import "@/assets/scss/mixin.scss";
+.searchPanel-results {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  margin: 3rem auto;
+  position: relative;
+
+  .filters-wrapper {
+    width: 20%;
+    will-change: width;
+  }
+
+  .hits-wrapper {
+    width: 100%;
+  }
+}
+//SORT BY and STATS
+
+.sort-and-stat {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin: -1rem auto 2rem auto;
+  .ais-Stats-text {
+    color: $sixth-color;
+  }
+}
+.ais-SortBy-select {
+  @include btn;
+  .ais-SortBy-option {
+    color: $sixth-color;
+    @include title;
+  }
+}
+
+//SEARCHRESULT
+
+.hits-list {
+    @include hits-list;
+
+    .hit-list {
+        @include hit-list;
+    }
+}
+
+.searchPanel-results {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 100%;
+    margin: 3rem auto;
+    position: relative;
+
+    .filters-wrapper {
+        width: 20%;
+        will-change: width;
+    }
+
+    .hits-wrapper {
+        width: 100%;
+    }
+}
+</style>
